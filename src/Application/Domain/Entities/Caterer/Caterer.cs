9540dde00d350b2804c7cf66746d310a327f50ad
tuananhvu1234 <tuananhcho1234@gmail.com>
2023-07-29 10:42:56 +0700

    CreateBaseProject

---
+++ src/Application/Domain/Entities/Caterer/Caterer.cs
@@ -0,0 +1,6 @@
+﻿
+namespace OnlineCaterer.Application.Entities;
+
+public class Caterer
+{
+}
---
+++ src/Application/Domain/Entities/Generic/BasicUserInfo.cs
@@ -0,0 +1,7 @@
+﻿
+namespace OnlineCaterer.Application.Domain.Entities;
+
+public class BasicUserInfo
+{
+    public int Id { get; set; }
+}
